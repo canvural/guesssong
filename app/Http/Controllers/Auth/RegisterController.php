63bb2e41bdfe1aceed03a9801510c637d809f05b
Can Vural <can9119@gmail.com>
2018-02-16 18:39:54 +0100

    Change redirect route for auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/categories';
 
     /**
      * Create a new controller instance.
