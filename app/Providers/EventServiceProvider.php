ca18e32a5ff77d726a98875888a1f5918b1ccb0d
Can Vural <can9119@gmail.com>
2018-02-18 22:05:30 +0100

    Remove event for updating score

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,9 +14,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        UserAnsweredRight::class => [
-            UpdateScore::class,
-        ],
+        
     ];
 
     /**
